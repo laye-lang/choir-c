61d8dacfe7f38d0d3f6afe5b1f578922ac7f730a
Local Atticus <github@nashiora.com>
2025-04-17 02:39:49 -0400

    Update tokens.inc, document ch_token_key

--- include/choir/core.h
+++ include/choir/core.h
@@ -30,21 +30,34 @@ typedef double float64;
 typedef size_t usize;
 typedef ptrdiff_t isize;
 
+/// @brief Describes under what circumstances a given keyword is available.
 typedef enum ch_token_key {
+    /// @brief Indicates a keyword is available in all variants of C.
     CH_TKKEY_C = 1 << 0,
+    /// @brief Indicates a keyword is available in all variants of Laye.
     CH_TKKEY_LAYE = 1 << 1,
+    /// @brief Indicates a keyword is introduced to C in C99.
     CH_TKKEY_C99 = 1 << 2,
+    /// @brief Indicates a keyword is introduced to C in C23.
     CH_TKKEY_C23 = 1 << 3,
+    /// @brief Indicates a keyword is available if GNU extensions are enabled.
     CH_TKKEY_GNU = 1 << 4,
+    /// @brief Indicates a keyword is available if Microsoft extensions are enabled.
     CH_TKKEY_MS = 1 << 5,
+    /// @brief Indicates a keyword is available if Microsoft compatibility mode is enabled.
     CH_TKKEY_MS_COMPAT = 1 << 6,
+    /// @brief Indicates a keyword is available if Clang extensions are enabled.
     CH_TKKEY_CLANG = 1 << 7,
+    /// @brief Indicates a keyword is available if Borland extensions are enabled.
     CH_TKKEY_BORLAND = 1 << 8,
+    /// @brief Indicates a keyword is available if Choir extensions are enabled.
     CH_TKKEY_CHOIR = 1 << 9,
+    /// @brief Indicates a keyword is available if 'bool' is a built-in type.
     CH_TKKEY_BOOL = 1 << 10,
-    CH_TKKEY_WCHAR = 1 << 11,
 
-    CH_TKKEY_MAX = CH_TKKEY_WCHAR,
+    /// @brief The maximum key flag value.
+    CH_TKKEY_MAX = CH_TKKEY_BOOL,
+    /// @brief Indicates a keyword is available in all variants of C and Laye.
     CH_TKKEY_ALL = (CH_TKKEY_MAX) | (CH_TKKEY_MAX - 1),
 } ch_token_key;
 
