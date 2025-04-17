a978105738a8436a86c50a482dfdb15b079aebfe
Local Atticus <github@nashiora.com>
2025-04-16 22:46:25 -0400

    Update CFLAGS in a Linux config

--- configurations/x86_64-unknown-linux-clang.h
+++ configurations/x86_64-unknown-linux-clang.h
@@ -3,7 +3,7 @@
 #define LD "clang"
 
 #define LCONFIG , "-DLAYE_USE_LINUX"
-#define CFLAGS "-std=c23", "-Wall", "-Werror", "-Werror=return-type", "-pedantic", "-pedantic-errors", "-ggdb", "-fsanitize=address" LCONFIG
+#define CFLAGS "-std=c23", "-Wall", "-Wextra", "-Wno-unused-parameter", "-Werror", "-Werror=return-type", "-pedantic", "-pedantic-errors", "-ggdb", "-fsanitize=address" LCONFIG
 #define LDFLAGS "-ggdb", "-fsanitize=address"
 
 #define EXE_EXT ""
